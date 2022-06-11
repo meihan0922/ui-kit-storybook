bed4f557091b3ca9ce59a97b03e88ef1cbef5f8f
Mei <meihen0922@gmail.com>
2022-06-11 20:52:13 +0800

    fix(main.js): addon-storysource會發生不明build type error，改用docs+controls，更改讀取的story路徑、resolve
    新增typescript規則

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,18 @@
 // .storybook/main.js
 const path = require("path");
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: [
+    "../src/components/**/*.stories.mdx",
+    "../src/components/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/addon-storysource",
+    // ERR_INVALID_ARG_TYPE
+    // "@storybook/addon-storysource",
+    "@storybook/addon-docs",
+    "@storybook/addon-controls",
     "@storybook/preset-create-react-app",
   ],
   webpackFinal: async (config) => {
@@ -23,11 +29,36 @@ module.exports = {
       ],
       include: path.resolve(__dirname, "../"),
     });
+    config.resolve = {
+      ...config.resolve,
+      extensions: [
+        ".js",
+        ".jsx",
+        ".mdx",
+        ".ts",
+        ".tsx",
+        ".json",
+        ".css",
+        ".scss",
+      ],
+      modules: ["src", "node_modules"], // Assuming that your files are inside the src dir
+    };
     return config;
   },
 
   framework: "@storybook/react",
   core: {
     builder: "webpack5",
+  },
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: "react-docgen-typescript",
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: {
+        skipNodeModules: true,
+      },
+    },
   },
 };
