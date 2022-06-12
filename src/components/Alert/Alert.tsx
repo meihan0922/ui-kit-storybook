39b03e0090a7e947c28b5fe52f51f859f046df10
Mei <meihen0922@gmail.com>
2022-06-12 14:15:59 +0800

    feat(alert): 新增alert

---
+++ src/components/Alert/Alert.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleCheck,
+  faCircleExclamation,
+  faCircleXmark,
+  faCircleQuestion,
+} from "@fortawesome/free-solid-svg-icons";
+import cx from "classnames";
+import { Color } from "../../type/color";
+
+type AlertType = "success" | "info" | "alert" | "warn";
+
+export interface IAlertProps {
+  /** alert type */
+  type?: AlertType | "none";
+  /** children */
+  children: React.ReactNode;
+}
+
+const Icon: { [key in AlertType]: JSX.Element } = {
+  success: (
+    <FontAwesomeIcon
+      size="lg"
+      color={Color["systemColors-success"]}
+      icon={faCircleCheck}
+    />
+  ),
+  info: (
+    <FontAwesomeIcon
+      size="lg"
+      color={Color["systemColors-info"]}
+      icon={faCircleExclamation}
+    />
+  ),
+  alert: (
+    <FontAwesomeIcon
+      size="lg"
+      color={Color["systemColors-alert"]}
+      icon={faCircleQuestion}
+    />
+  ),
+  warn: (
+    <FontAwesomeIcon
+      size="lg"
+      color={Color["systemColors-error"]}
+      icon={faCircleXmark}
+    />
+  ),
+};
+
+export const Alert = ({ type = "success", children }: IAlertProps) => {
+  return (
+    <div className="px-4 py-5 max-w-[350px] flex rounded bg-styleColors-lightBlue text-styleColors-mainGray">
+      {type !== "none" && <div className="mr-4">{Icon[type]}</div>}
+      {children}
+    </div>
+  );
+};
