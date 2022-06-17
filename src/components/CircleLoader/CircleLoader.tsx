a8a4262540fd0f6ff51019be76aa0fef9536fceb
Mei <meihen0922@gmail.com>
2022-06-17 16:38:04 +0800

    feat(circleloader): 新增circleLoader元件

---
+++ src/components/CircleLoader/CircleLoader.tsx
@@ -0,0 +1,31 @@
+import { motion } from "framer-motion";
+
+export const CircleLoader = ({
+  width = "2rem",
+  height = "2rem",
+}: {
+  width?: string;
+  height?: string;
+}) => {
+  return (
+    <div
+      className="relative"
+      style={{
+        width,
+        height,
+      }}
+    >
+      <motion.div
+        className="w-full h-full border-4 border-styleColors-gray  border-t-chartColor-mainBlue rounded-[50%] absolute top-0 left-0"
+        animate={{ rotate: 360 }}
+        transition={{
+          loop: Infinity,
+          duration: 1,
+          ease: "linear",
+        }}
+      />
+    </div>
+  );
+};
+
+export default CircleLoader;
